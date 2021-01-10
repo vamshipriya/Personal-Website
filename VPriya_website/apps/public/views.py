b3dbf2e1aa2b24c2fa8f30cf712ac4cc948a32ec
vamshipriya <priya.nizamkari@gmail.com>
2021-01-10 15:10:35 +0530

    added basic html pages

---
+++ VPriya_website/apps/public/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render
+
+
+def index(request:HttpRequest) -> HttpResponse:
+    return render(request, 'index.html')
+
+def about(request:HttpRequest) -> HttpResponse:
+    return render(request, 'about.html')
+
+def contact(request:HttpRequest) -> HttpResponse:
+    return render(request, 'contact.html')
